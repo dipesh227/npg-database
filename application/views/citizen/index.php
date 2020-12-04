510ac91ec6f94f8443a38d52a2e3f317bceb774e
dipesh227 <dipeshjoshi227@gmail.com>
2020-12-04 08:45:02 +0530

    nw

--- application/views/citizen/index.php
+++ application/views/citizen/index.php
@@ -4,34 +4,7 @@ $this->load->view('./includes/header.php');
 ?>
 <div class="container-fluid mt-2">
     <div class="row">
-        <div class="col-md-2 col-sm-2 text-center sidebar-nav">
-            <!-- Nav tabs -->
-            <ul class="nav nav-tabs flex-column" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active outline" data-toggle="tab" href="#home">Home</a>
-                </li>
-                <li class="nav-item">
-                    <div id="accordion">
-                        <a class="collapsed nav-link" data-toggle="collapse" href="#collapseTwo">
-                            PFMS
-                        </a>
-                        <div id="collapseTwo" class="collapse" data-parent="#accordion">
-                            <ul class="nav nav-tabs flex-column" role="tablist">
-                                <li class="nav-item">
-                                    <a class="nav-link" data-toggle="tab" href="#pfmstab">P.F.M.S Aawash</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" data-toggle="tab" href="#menu2">Menu 2</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#menu2">Menu 2</a>
-                </li>
-            </ul>
-        </div>
+    <?php $this->load->view('./citizen/sidebar.php'); ?>
         <div class="col-md-10 col-sm-10">
             <!-- Tab panes -->
             <div class="tab-content">
@@ -47,7 +20,6 @@ $this->load->view('./includes/header.php');
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 <?php
---
+++ application/views/citizen/sidebar.php
@@ -0,0 +1,26 @@
+<div class="col-md-2 col-sm-2 text-center sidebar-nav">
+    <!-- Nav tabs -->
+    <ul class="nav nav-tabs flex-column" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active outline" data-toggle="tab" href="#home">Home</a>
+        </li>
+        <li class="nav-item" id="accordion">
+            <a class="collapsed nav-link" data-toggle="collapse" href="#collapseTwo" >
+                PFMS
+            </a>
+            <div id="collapseTwo" class="collapse nav nav-tabs flex-column" data-parent="#accordion">
+                <ul class="nav nav-tabs flex-column" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" href="#pfmstab">P.F.M.S Aawash</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-toggle="tab" href="#menu2">Menu 2</a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#menu2">Menu 2</a>
+        </li>
+    </ul>
+</div>
